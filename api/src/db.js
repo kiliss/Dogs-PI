b3e88b43f9b3ec7428b19330eed2e86f726961e8
kiliss <fabigus14@gmail.com>
2022-08-23 12:22:17 -0300

    Changes

--- api/src/db.js
+++ api/src/db.js
@@ -30,7 +30,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
         { logging: false, native: false }
       );                                                                  
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
