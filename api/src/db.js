1d0a421c12122ef49ca47820f3dd4de0a4aa3eb2
kiliss <fabigus14@gmail.com>
2022-10-11 21:09:52 -0300

    Cambio de puerto

--- api/src/db.js
+++ api/src/db.js
@@ -11,7 +11,7 @@ let sequelize =
         database: DB_NAME,
         dialect: "postgres",
         host: DB_HOST,
-        port: 7558,
+        port: 5822,
         username: DB_USER,
         password: DB_PASSWORD,
         pool: {
